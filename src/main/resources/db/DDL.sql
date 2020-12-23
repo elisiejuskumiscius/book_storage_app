8fd66fd691370b353ca4606662e3199011a02b7a
elisiejus <ekumiscius@gmail.com>
2020-12-24 00:48:42 +0200

    DB update

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -9,7 +9,7 @@ CREATE TABLE book
     id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     author VARCHAR(50) NOT NULL,
-    barcode CHAR(13) NOT NULL UNIQUE,
+    barcode VARCHAR(13) NOT NULL UNIQUE,
     quantity INT UNSIGNED,
     price_per_unit DECIMAL(10,2) NOT NULL
 );
@@ -19,10 +19,10 @@ CREATE TABLE antique_book
     id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     author VARCHAR(50) NOT NULL,
-    barcode CHAR(13) NOT NULL UNIQUE,
+    barcode VARCHAR(13) NOT NULL UNIQUE,
     quantity INT UNSIGNED,
     price_per_unit DECIMAL(10,2) NOT NULL,
-    release_year DATE
+    release_year DATE NOT NULL
 );
 
 CREATE TABLE science_journal
@@ -30,7 +30,7 @@ CREATE TABLE science_journal
     id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     author VARCHAR(50) NOT NULL,
-    barcode CHAR(13) NOT NULL UNIQUE,
+    barcode VARCHAR(13) NOT NULL UNIQUE,
     quantity INT UNSIGNED,
     price_per_unit DECIMAL(10,2) NOT NULL,
     science_index TINYINT NOT NULL
