714ff41ff8e2726d3da8c6a4d27e441b2b7a400c
elisiejus <ekumiscius@gmail.com>
2020-12-23 03:51:38 +0200

    Initial commit

---
+++ src/test/java/book/storage/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package book.storage.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
